6ff5d2b5022548f5310e40093e15dcb7847726a2
miku0129 <svartkatt39@gmail.com>
2023-10-19 18:52:22 +0200

    add importing css to app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import { ProjectInput } from "./components/project-input";
 import { ProjectList } from "./components/project-list";
 
+import './app.css'
+
 new ProjectInput();
 new ProjectList("active");
 new ProjectList("finished");
