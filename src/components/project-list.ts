0b34a792c60d1a62f7c8bc18610268efc9bb2c33
miku0129 <svartkatt39@gmail.com>
2023-10-19 15:41:48 +0200

    add webpack to the project

--- src/components/project-list.ts
+++ src/components/project-list.ts
@@ -1,10 +1,10 @@
-import { Dragtarget } from "../models/drag-drop.js";
-import { Project } from "../models/project.js";
-import Component from "./base-component.js";
-import { autobind } from "../decorators/autobind.js";
-import { projectState } from "../state/project-state.js";
-import { ProjectItem } from "./project-item.js";
-import { ProjectStatus } from "../models/project.js";
+import { Dragtarget } from "../models/drag-drop";
+import { Project } from "../models/project";
+import Component from "./base-component";
+import { autobind } from "../decorators/autobind";
+import { projectState } from "../state/project-state";
+import { ProjectItem } from "./project-item";
+import { ProjectStatus } from "../models/project";
 
 // ProjectList Class
 export class ProjectList
